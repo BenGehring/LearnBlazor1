8b4e83377f42b896e197697e712b86f0ab23f8fb
Ben <ben@bengehring.com>
2025-07-26 19:05:34 -0400

    Completed section 2 and started section 3

---
+++ LearnBlazor1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnBlazor1.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public IEnumerable<ProductProperty> ProductProperties { get; set; } = [];
+}
